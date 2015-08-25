8bd8bba088995f30888f9a88fb2581476f716c1e
Xavier <mawet.xavier@gmail.com>
2015-08-26 00:23:01 +0200

    Added a drop message in DragDropPanelWithTitle

--- src/main/java/dragdrop/DragDropPanelWithTitle.java
+++ src/main/java/dragdrop/DragDropPanelWithTitle.java
@@ -6,6 +6,9 @@
 package dragdrop;
 
 import java.awt.BorderLayout;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
@@ -16,7 +19,10 @@ import javax.swing.SwingConstants;
 public class DragDropPanelWithTitle extends DragDropPanel
 {
     //<editor-fold defaultstate="collapsed" desc="Variables declaration">
-    private JLabel labelTitle;
+    protected JLabel labelMessage;
+
+    protected String title;
+    protected String dropMessage;
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="Constructors">
@@ -27,14 +33,45 @@ public class DragDropPanelWithTitle extends DragDropPanel
 
     public DragDropPanelWithTitle(String title)
     {
+        this(title, "");
+    }
+
+    public DragDropPanelWithTitle(String title, String dropMessage)
+    {
         // GUI Configuration
         super();
         this.setLayout(new java.awt.BorderLayout());
 
         // Populate panel
-        this.labelTitle = new JLabel(title);
-        this.labelTitle.setHorizontalAlignment(SwingConstants.CENTER);
-        this.add(this.labelTitle, BorderLayout.CENTER);
+        this.labelMessage = new JLabel(title);
+        this.labelMessage.setHorizontalAlignment(SwingConstants.CENTER);
+        this.add(this.labelMessage, BorderLayout.CENTER);
+
+        this.title = title;
+        this.dropMessage = dropMessage.isEmpty() ? title : dropMessage;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Overrided methods">
+    @Override
+    public void dragEnter(DropTargetDragEvent dtde)
+    {
+        this.labelMessage.setText(this.dropMessage);
+        super.dragEnter(dtde);
+    }
+
+    @Override
+    public void dragExit(DropTargetEvent dte)
+    {
+        this.labelMessage.setText(this.title);
+        super.dragExit(dte);
+    }
+
+    @Override
+    public void drop(DropTargetDropEvent dtde)
+    {
+        this.labelMessage.setText(this.title);
+        super.drop(dtde);
     }
     //</editor-fold>
 }
