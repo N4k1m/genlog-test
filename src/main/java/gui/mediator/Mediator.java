0c84680608c42bed9e2cc0eb1ac9b2da2eda070e
Xavier <mawet.xavier@gmail.com>
2015-08-26 16:21:58 +0200

    Added Colleague and Mediator interfaces

---
+++ src/main/java/gui/mediator/Mediator.java
@@ -0,0 +1,15 @@
+/*
+ * Institut Supérieur Industriel Liégeois - Département ingénieurs industriels
+ * Copyright 2015 Mawet Xavier. All rights reserved.
+ * http://www.nakim.be
+ */
+package gui.mediator;
+
+/**
+ *
+ * @author Nakim
+ */
+public interface Mediator
+{
+
+}
